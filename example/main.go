f689698a41e5f860853cc27ab9994fba540a37c4
Javier Provecho Fernandez <javiertitan@gmail.com>
2016-05-06 04:38:46 +0200

    change returned struct type

--- example/main.go
+++ example/main.go
@@ -7,11 +7,17 @@ import (
 )
 
 func main() {
-	json, err := prom2json.Parse("http://146.185.151.73:9100/metrics")
+	metrics, err := prom2json.FetchMetricFamilies("http://146.185.151.73:9100/metrics")
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%s", json["node_network_transmit_bytes"])
+	fmt.Printf(
+		"%f",
+		metrics["node_network_transmit_bytes"].
+			GetMetric()[0].
+			GetGauge().
+			GetValue(),
+	)
 }
--- parser.go
+++ parser.go
@@ -15,7 +15,6 @@ package prom2json
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 
 	"github.com/prometheus/common/expfmt"
@@ -23,106 +22,8 @@ import (
 	dto "github.com/prometheus/client_model/go"
 )
 
-// MetricFamily is a collection of metrics
-type MetricFamily struct {
-	Name    string        `json:"name"`
-	Help    string        `json:"help"`
-	Type    string        `json:"type"`
-	Metrics []interface{} `json:"metrics,omitempty"` // Either metric or summary.
-}
-
-// Metric is single-value metric
-type Metric struct {
-	Labels map[string]string `json:"labels,omitempty"`
-	Value  string            `json:"value"`
-}
-
-// Summary is a multiple-value metric
-type Summary struct {
-	Labels    map[string]string `json:"labels,omitempty"`
-	Quantiles map[string]string `json:"quantiles,omitempty"`
-	Count     string            `json:"count"`
-	Sum       string            `json:"sum"`
-}
-
-type Histogram struct {
-	Labels  map[string]string `json:"labels,omitempty"`
-	Buckets map[string]string `json:"buckets,omitempty"`
-	Count   string            `json:"count"`
-	Sum     string            `json:"sum"`
-}
-
-func newMetricFamily(dtoMF *dto.MetricFamily) *MetricFamily {
-	mf := &MetricFamily{
-		Name:    dtoMF.GetName(),
-		Help:    dtoMF.GetHelp(),
-		Type:    dtoMF.GetType().String(),
-		Metrics: make([]interface{}, len(dtoMF.Metric)),
-	}
-	for i, m := range dtoMF.Metric {
-		if dtoMF.GetType() == dto.MetricType_SUMMARY {
-			mf.Metrics[i] = Summary{
-				Labels:    makeLabels(m),
-				Quantiles: makeQuantiles(m),
-				Count:     fmt.Sprint(m.GetSummary().GetSampleCount()),
-				Sum:       fmt.Sprint(m.GetSummary().GetSampleSum()),
-			}
-		} else if dtoMF.GetType() == dto.MetricType_HISTOGRAM {
-			mf.Metrics[i] = Histogram{
-				Labels:  makeLabels(m),
-				Buckets: makeBuckets(m),
-				Count:   fmt.Sprint(m.GetHistogram().GetSampleCount()),
-				Sum:     fmt.Sprint(m.GetSummary().GetSampleSum()),
-			}
-		} else {
-			mf.Metrics[i] = Metric{
-				Labels: makeLabels(m),
-				Value:  fmt.Sprint(getValue(m)),
-			}
-		}
-	}
-	return mf
-}
-
-func getValue(m *dto.Metric) float64 {
-	if m.Gauge != nil {
-		return m.GetGauge().GetValue()
-	}
-	if m.Counter != nil {
-		return m.GetCounter().GetValue()
-	}
-	if m.Untyped != nil {
-		return m.GetUntyped().GetValue()
-	}
-	return 0.
-}
-
-func makeLabels(m *dto.Metric) map[string]string {
-	result := map[string]string{}
-	for _, lp := range m.Label {
-		result[lp.GetName()] = lp.GetValue()
-	}
-	return result
-}
-
-func makeQuantiles(m *dto.Metric) map[string]string {
-	result := map[string]string{}
-	for _, q := range m.GetSummary().Quantile {
-		result[fmt.Sprint(q.GetQuantile())] = fmt.Sprint(q.GetValue())
-	}
-	return result
-}
-
-func makeBuckets(m *dto.Metric) map[string]string {
-	result := map[string]string{}
-	for _, b := range m.GetHistogram().Bucket {
-		result[fmt.Sprint(b.GetUpperBound())] = fmt.Sprint(b.GetCumulativeCount())
-	}
-	return result
-}
-
-func fetchMetricFamilies(url string) ([]*dto.MetricFamily, error) {
-
+// FetchMetricFamilies returns an map of unmarshalled metrics
+func FetchMetricFamilies(url string) (map[string]*dto.MetricFamily, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -139,45 +40,5 @@ func fetchMetricFamilies(url string) ([]*dto.MetricFamily, error) {
 		return nil, err
 	}
 
-	result := []*dto.MetricFamily{}
-	for _, metric := range metrics {
-		result = append(result, metric)
-	}
-
-	return result, nil
-}
-
-// Parse receives a prometheus metric url and return a parsed json string
-func Parse(url string) (map[string][]string, error) {
-	metrics, err := fetchMetricFamilies(url)
-
-	if err != nil {
-		return nil, err
-	}
-
-	response := []*MetricFamily{}
-	for _, mf := range metrics {
-		response = append(response, newMetricFamily(mf))
-	}
-
-	result := make(map[string][]string)
-
-	for _, entry := range response {
-		for _, metric := range entry.Metrics {
-			if w, ok := metric.(Metric); ok {
-				result[entry.Name] = append(result[entry.Name], w.Value)
-				continue
-			}
-			if w, ok := metric.(Summary); ok {
-				result[entry.Name] = append(result[entry.Name], w.Sum)
-				continue
-			}
-		}
-	}
-
-	if result == nil {
-		return nil, errors.New("result is nil")
-	}
-
-	return result, nil
+	return metrics, nil
 }
